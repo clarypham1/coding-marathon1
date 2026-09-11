5400209cd5d908f6168751054d0fd1d46e2f3934
Lehtonen Saga <sagalehtonen@gmail.com>
2026-09-11 21:46:11 +0300

    fix app.jsx after merge
    imports

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import BookCollection from './BookCollectionManager/BookCollection'
+import SignupPage from './SignupPage/SignupPage'
+import RecipeManager from './RecipeManager/RecipeManager'
+import ShoppingCart from './ShoppinCart/ShoppingCart'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +10,10 @@ function App() {
   return (
     <>
       <BookCollection />
+      <SignupPage />
+      <RecipeManager />
+      <ShoppingCart />
+      <ContactListManager />
     </>
   )
 }
