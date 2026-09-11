4626a5e803c6739b273016ca888a5853e1f50e2d
Lehtonen Saga <sagalehtonen@gmail.com>
2026-09-11 21:49:18 +0300

    Fix import path for ShoppingCart component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import BookCollection from './BookCollectionManager/BookCollection'
 import SignupPage from './SignupPage/SignupPage'
 import RecipeManager from './RecipeManager/RecipeManager'
-import ShoppingCart from './ShoppinCart/ShoppingCart'
+import ShoppingCart from './ShoppinCart'
 
 function App() {
   const [count, setCount] = useState(0)
